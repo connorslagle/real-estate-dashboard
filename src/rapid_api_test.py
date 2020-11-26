c93147482e020eea948593432909c961749f18e2
connorslagle <connor.slagle@colorado.edu>
2020-11-26 14:51:37 -0700

    b4 turkey

--- src/rapid_api_test.py
+++ src/rapid_api_test.py
@@ -2,22 +2,39 @@ import requests
 import os
 import pprint
 
-
-if __name__ == "__main__":
+def listings_query(city="Denver", limit='200'):
+    '''
+    Query Realtor API using RapidAPI
+    '''
     url = "https://realtor.p.rapidapi.com/properties/v2/list-for-sale"
-
-    querystring = {"city":"Denver","limit":"1","offset":"0","state_code":"CO","sort":"relevance"}
-
+    querystring = {"city":city,"limit":limit,"offset":"0","state_code":"CO","sort":"relevance"}
     api_key = os.environ['RAPID_API_KEY_REALTOR']
 
     headers = {
         'x-rapidapi-key': api_key,
         'x-rapidapi-host': "realtor.p.rapidapi.com"
         }
-
+    
     response = requests.request("GET", url, headers=headers, params=querystring)
-    test = response.json()
+    return response.json()
 
+def details_query(property_id):
+    '''
+    Query Realtor API using RapidAPI, details API
+    '''
+    url = "https://realtor.p.rapidapi.com/properties/v2/list-for-sale"
+    querystring = {"city":city,"limit":limit,"offset":"0","state_code":"CO","sort":"relevance"}
+    api_key = os.environ['RAPID_API_KEY_REALTOR']
+
+    headers = {
+        'x-rapidapi-key': api_key,
+        'x-rapidapi-host': "realtor.p.rapidapi.com"
+        }
+    
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return response.json()
+
+if __name__ == "__main__":
     prop_id = test.get('properties')[0].get('property_id')
 
     detail_query = {"property_id":prop_id}
