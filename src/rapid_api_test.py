eefe10ca326de0db22c866dd43497874f942c0bc
connorslagle <connor.slagle@colorado.edu>
2020-11-26 16:08:25 -0700

    pymongo started

--- src/rapid_api_test.py
+++ src/rapid_api_test.py
@@ -1,10 +1,11 @@
 import requests
 import os
 import pprint
+import pymongo
 
 def listings_query(city="Denver", limit='200'):
     '''
-    Query Realtor API using RapidAPI
+    Query Realtor listings API using RapidAPI
     '''
     url = "https://realtor.p.rapidapi.com/properties/v2/list-for-sale"
     querystring = {"city":city,"limit":limit,"offset":"0","state_code":"CO","sort":"relevance"}
@@ -20,10 +21,10 @@ def listings_query(city="Denver", limit='200'):
 
 def details_query(property_id):
     '''
-    Query Realtor API using RapidAPI, details API
+    Query Realtor details API using RapidAPI, details API
     '''
-    url = "https://realtor.p.rapidapi.com/properties/v2/list-for-sale"
-    querystring = {"city":city,"limit":limit,"offset":"0","state_code":"CO","sort":"relevance"}
+    url = "https://realtor.p.rapidapi.com/properties/v2/detail"
+    querystring = {"property_id":property_id}
     api_key = os.environ['RAPID_API_KEY_REALTOR']
 
     headers = {
@@ -34,16 +35,27 @@ def details_query(property_id):
     response = requests.request("GET", url, headers=headers, params=querystring)
     return response.json()
 
+class mongoImporter():
+    def __init__(self):
+        self.mongo_client = pymongo.MongoClient('localhost', 27017)
+        self.mongo_db = self.mongo_client['RealtorStore']
+
+    def store_listings(self, listing_json):
+        col = self.mongo_db['listings']
+        col.insert(listing_json)
+    
+    def store_details(self, details_json):
+        col = self.mongo_db['details']
+        col.insert(details_json)
+    
+
 if __name__ == "__main__":
-    prop_id = test.get('properties')[0].get('property_id')
-
-    detail_query = {"property_id":prop_id}
-    detail_url = "https://realtor.p.rapidapi.com/properties/v2/detail"
-
-    detail_response = requests.request("GET", detail_url, headers=headers, params=detail_query)
-    details = detail_response.json()
-    pprint.pprint(details)
+    # prop_id = test.get('properties')[0].get('property_id')
 
     '''
     Wednesday November 25, 2020: Can access photos using deatils api. Will need to test if I can dl.
     '''
+    listing_json = listings_query()
+
+    importer = mongoImporter()
+    importer.store_listings(listing_json)
